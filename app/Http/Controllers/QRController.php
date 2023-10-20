0f99bdd909937235a3d9d3273df0baca3d0000bf
helloteam-developer1 <helloteam.developer1@gmail.com>
2023-10-19 21:02:21 -0600

    Nuevos QR

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 class QRController extends Controller
 {
     public function qr_code(){
-        
-    
+
         QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
         ->eyeColor(1, 109, 2, 2,0, 0, 0)
         ->eyeColor(2, 109, 2, 2,0, 0, 0)
@@ -17,7 +16,7 @@ class QRController extends Controller
         ->format('png')
         ->size(2000)
         ->merge(public_path('img/600KIA.png'), .1,true)
-        ->generate('https://documentos-kia.web.app/ForteHB.pdf', '../public/qrcodes/ForteHBqrcode.png' );
+        ->generate('https://documentos-kia.firebaseapp.com/EV6_2024.pdf', '../public/qrcodes/EV6_2024.png' );
         QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
         ->eyeColor(1, 109, 2, 2,0, 0, 0)
         ->eyeColor(2, 109, 2, 2,0, 0, 0)
@@ -25,7 +24,7 @@ class QRController extends Controller
         ->format('png')
         ->size(2000)
         ->merge(public_path('img/600KIA.png'), .1,true)
-        ->generate('https://documentos-kia.web.app/ForteSeda.pdf', '../public/qrcodes/ForteSedaqrcode.png' );
+        ->generate('https://documentos-kia.firebaseapp.com/FORTE.pdf', '../public/qrcodes/FORTE.png' );
         QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
         ->eyeColor(1, 109, 2, 2,0, 0, 0)
         ->eyeColor(2, 109, 2, 2,0, 0, 0)
@@ -33,7 +32,23 @@ class QRController extends Controller
         ->format('png')
         ->size(2000)
         ->merge(public_path('img/600KIA.png'), .1,true)
-        ->generate('https://documentos-kia.web.app/Niro.pdf', '../public/qrcodes/Niroqrcode.png' );
+        ->generate('https://documentos-kia.firebaseapp.com/Niro_2023.pdf', '../public/qrcodes/NIRO_2023.png' );
+        QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
+        ->eyeColor(1, 109, 2, 2,0, 0, 0)
+        ->eyeColor(2, 109, 2, 2,0, 0, 0)
+        ->style('round')
+        ->format('png')
+        ->size(2000)
+        ->merge(public_path('img/600KIA.png'), .1,true)
+        ->generate('https://documentos-kia.firebaseapp.com/Seltos_2024.pdf', '../public/qrcodes/SELTOS_2024.png' );
+        QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
+        ->eyeColor(1, 109, 2, 2,0, 0, 0)
+        ->eyeColor(2, 109, 2, 2,0, 0, 0)
+        ->style('round')
+        ->format('png')
+        ->size(2000)
+        ->merge(public_path('img/600KIA.png'), .1,true)
+        ->generate('https://documentos-kia.firebaseapp.com/Kia_K3.pdf', '../public/qrcodes/KIA_K3.png' );
         QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
         ->eyeColor(1, 109, 2, 2,0, 0, 0)
         ->eyeColor(2, 109, 2, 2,0, 0, 0)->margin(2)
@@ -41,7 +56,7 @@ class QRController extends Controller
         ->format('png')
         ->size(2000)
         ->merge(public_path('img/600KIA.png'), .1,true)
-        ->generate('https://documentos-kia.web.app/Seltos.pdf', '../public/qrcodes/Seltosqrcode.png' );
+        ->generate('https://documentos-kia.firebaseapp.com/SORENTO_Septiembre_2023.pdf', '../public/qrcodes/SORENTO_2023.png' );
         QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
         ->eyeColor(1, 109, 2, 2,0, 0, 0)
         ->eyeColor(2, 109, 2, 2,0, 0, 0)
@@ -49,7 +64,7 @@ class QRController extends Controller
         ->format('png')
         ->size(2000)
         ->merge(public_path('img/600KIA.png'), .1,true)
-        ->generate('https://documentos-kia.web.app/Sorento.pdf', '../public/qrcodes/Sorentoqrcode.png' );
+        ->generate('https://documentos-kia.firebaseapp.com/SPORTAGE_2023.pdf', '../public/qrcodes/SPORTAGE_2023.png' );
         QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
         ->eyeColor(1, 109, 2, 2,0, 0, 0)
         ->eyeColor(2, 109, 2, 2,0, 0, 0)->margin(2)
@@ -57,16 +72,8 @@ class QRController extends Controller
         ->format('png')
         ->size(2000)
         ->merge(public_path('img/600KIA.png'), .1,true)
-        ->generate('https://documentos-kia.web.app/Soul.pdf', '../public/qrcodes/Soulqrcode.png' );
-       QrCode::eye('circle')->margin(2)->eyeColor(0, 109, 2, 2,0, 0, 0)
-        ->eyeColor(1, 109, 2, 2,0, 0, 0)
-        ->eyeColor(2, 109, 2, 2,0, 0, 0)->margin(2)
-        ->style('round')
-        ->format('png')
-        ->size(2000)
-        ->merge(public_path('img/600KIA.png'), .1,true)
-        ->generate('https://documentos-kia.web.app/Sportage.pdf', '../public/qrcodes/Sportageqrcode.png' );
-        
+        ->generate('https://documentos-kia.firebaseapp.com/Soul_2023.pdf', '../public/qrcodes/Soulqrcode.png' );
+
 
 
 
